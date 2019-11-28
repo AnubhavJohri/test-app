a74c98dff3ce0f52cf761189384c19ffb282eee1
anubhavjohri <anubhavj22@gmail.com>
2019-11-28 14:06:12 +0530

    Made some tweeks

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
-import {React , Fragment} from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {BrowserRouter as Router , Redirect ,Switch , Route} from 'react-dom';
-import Home from "./components/Home";
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch ,Redirect} from 'react-router-dom';
 import './App.css';
+import Home from "./components/Home";
 
-function App() {
-  return (
-    <Router>
-      <Fragment>
-        <Switch>
-          <Route exact path='/home' component={Home} />
-          <Route exact path='/**' render={()=><Redirect to="/home"/>} />
-        </Switch>
-      </Fragment>
-    </Router>
-  );
+
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <Router>
+          <Fragment>
+            <Switch>
+              <Route exact path='/home' component={Home} />
+              <Route exact path='/**' render={()=><Redirect to="/home"/>} />
+            </Switch>
+          </Fragment>
+        </Router>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App
