09792016954d269e0e236ccb449219d2b920fb00
anubhavjohri <anubhavj22@gmail.com>
2019-11-28 13:41:10 +0530

    Created Home Component

---
+++ src/components/Home.js
@@ -0,0 +1,30 @@
+import React from "react" ;
+// var gcloud = require('gcloud') ;
+
+export default class Home extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {}
+    }
+
+
+    render(){
+        return(
+            <React.Fragment>
+                <h1 className="display-2">Hi there!</h1>
+            </React.Fragment>
+        )
+    }
+}
+
+// ({
+//     keyFilename: '/path/to/keyfile.json',
+//     projectId: '<YOUR-PROJECT-HERE>'
+//   });
+//   var vision = gcloud.vision();
+  
+//   var image = 'image.jpg';
+  
+//   vision.detectText('image.jpg', function(err, text, apiResponse) {
+//     // text = ['This was text found in the image']
+//   });
